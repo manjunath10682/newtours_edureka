2a3dd9716150ca64cf4cfdbb0cd849430a49c11c
manjunath ramareddy <manjunath.ramareddy@gmail.com>
2020-02-17 17:20:04 +0530

    fixed test failures

--- src/main/java/manjunath/newtours/newtoursTests.java
+++ src/main/java/manjunath/newtours/newtoursTests.java
@@ -14,18 +14,6 @@ import org.testng.annotations.Test;
 public class newtoursTests {
 
 	public WebDriver driver;
-	//public WebDriver driver = new FirefoxDriver();
-
-	//@BeforeMethod
-	//public void launchBrowser() {
-		//System.setProperty("webdriver.chrome.driver", "D:\\selenium_drivers\\chromedriver.exe");
-		//System.setProperty("webdriver.gecko.driver", "D:\\selenium_drivers\\geckodriver.exe");
-		//WebDriver driver = new ChromeDriver();
-		//driver.manage().window().maximize();
-		//Reporter.log("Starting the applicaton", true);
-		//driver.get("https://www.google.co.in/");
-	//}
-
 	@Test
 	public void Login() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\m1014807\\eclipse-workspace\\Automation\\libs\\chromedriver.exe");
@@ -41,26 +29,29 @@ public class newtoursTests {
 		System.out.println("Login successful");
 		driver.close();
 	}
-	
 	@Test
-	public void FlightBooking() throws InterruptedException {
+	public void FlightsBooking() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\m1014807\\eclipse-workspace\\Automation\\libs\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		Reporter.log("Starting the applicaton", true);
 		driver.get("http://www.newtours.demoaut.com/");
 		Reporter.log("Flight booking process started", true);
-		driver.findElement(By.xpath("//a[text()='Flights")).click();
-		//driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[1]/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[2]/td[2]/a")).click();
-		driver.findElement(By.xpath("//input[@name='findFlights']")).click();	
-		driver.findElement(By.xpath("//input[@name='reserveFlights']")).click();	
-		driver.findElement(By.xpath("//input[@name='passFirst0']")).sendKeys("TestMember1");	
-		driver.findElement(By.xpath("//input[@name='passLast0']")).sendKeys("Test");	
+		driver.findElement(By.xpath("//a[text()='Cruises']")).click();		
+		driver.findElement(By.xpath("//img[@alt='Reservations Open']")).click();		
+		driver.findElement(By.xpath("//input[@name='userName']")).sendKeys("mercury");		
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("mercury");		
+		driver.findElement(By.xpath("//input[@name='login']")).click();		
+		driver.findElement(By.xpath("//input[@name='findFlights']")).click();		
+		driver.findElement(By.xpath("//input[@name='reserveFlights']")).click();		
+		driver.findElement(By.xpath("//input[@name='passFirst0']")).sendKeys("TestMember1");		
+		driver.findElement(By.xpath("//input[@name='passLast0']")).sendKeys("Test");		
 		driver.findElement(By.xpath("//input[@name='buyFlights']")).click();
 		Thread.sleep(5000);
-		System.out.println("Flight booking is successful");
+		System.out.println("Flight booking is successful");	
 		driver.close();
 	}
+	
 	
 	@Test
 	public void CruiseBooking() throws InterruptedException {
